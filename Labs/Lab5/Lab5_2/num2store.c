9cbe75d3286ce9b35bca5ab3983d5743851743b9
jaspack <141270994+jaspack@users.noreply.github.com>
2024-04-17 14:07:59 -0400

    Update num2store.c

--- Labs/Lab5/Lab5_2/num2store.c
+++ Labs/Lab5/Lab5_2/num2store.c
@@ -1,1 +1,16 @@
-asfb
+
+
+char* Num2Store(int number)
+{
+    var temp = number;
+    int dividing = 1;
+    while (dividing)
+    {
+        int place = 0;
+        int working =1;
+        while (working)
+        {
+            
+            } 
+    }
+}
