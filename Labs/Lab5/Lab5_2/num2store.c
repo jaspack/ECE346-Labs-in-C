705134cc8dd48f5d42c78b31cfdf024aebbb9d3c
jaspack <141270994+jaspack@users.noreply.github.com>
2024-04-17 14:25:14 -0400

    Update num2store.c

--- Labs/Lab5/Lab5_2/num2store.c
+++ Labs/Lab5/Lab5_2/num2store.c
@@ -2,15 +2,38 @@
 
 char* Num2Store(int number)
 {
+    char *str = malloc(10);
     var temp = number;
-    int dividing = 1;
-    while (dividing)
+    int index = 0;
+    while (index < 9)
     {
         int place = 0;
         int working =1;
         while (working)
         {
-            
+            if ((temp /10) > 9) 
+            {
+                temp /= 10;
+                place++;
+            }
+            else
+            {
+                str[index] = ( temp + '0');
+                index++;
+                working = 0;
             } 
+        }
+        if (place != 0)
+        {
+            int ten = 10;
+            ten ^= place;
+            temp *= ten;
+            temp = number - temp;
+        }
+        else
+        {
+            str[index] = '\0';
+            return str;
+        }
     }
 }
